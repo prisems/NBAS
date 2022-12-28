63e467992a007d9e767676c5a806151939565529
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-12-28 10:36:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -78,7 +78,7 @@ int main() {
   int add1, add2, sub1, sub2, mult1, mult2, div1, div2, wait, errors, pow1,
       pow2;
   string custom = "false";
-  string version = "7.7.09";
+  string version = "7.8.11";
   string customCOLOR = "WHITE '0m'";
   int loop = 1;
   int prnt = 0;
