189eb6df684c302cec76b1082a976a22223080db
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-12-29 17:15:19 -0600

    installation

--- installation.sh
+++ installation.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/EarthRulerr/NBAS
 # Creates a local clone of the repository, including useless files.
+# After cloning the Repository you may delete all files excluding 'main.cpp', make sure to delete any other isntance of main.cpp that may already exist. Once only the main.cpp file is all set you may run the program and start programming in the console.
+# PS* You do jot have to delete everything after cloning, as long as main.cpp is the only isntance of main and is working, the program should work.
 # You can also run Notepad Basic code in your browser using the web compiler, see 'Installation' on the ReadMe and click the link.
