2056590105483d532b90552c57f4365c4b260082
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-12-29 17:11:15 -0600

    installation

--- installation.sh
+++ installation.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/EarthRulerr/NBAS
 # Creates a local clone of the repository, including useless files.
+# You can also run Notepad Basic code in your browser using the web compiler, see 'Installation' on the ReadMe and click the link.
